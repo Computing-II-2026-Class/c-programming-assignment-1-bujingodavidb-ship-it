bf017ea8829a10a0e53f14deb79cc0ca69ab8135
bujingodavidb-ship-it <bujingodavidb@gmail.com>
2026-03-24 19:23:54 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,26 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Bujingo David Kabanda */
+/* Student Number: 25/U/BIE/01364/BIE */
 
 #include <stdio.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main() {
+    double amount, fee_percentage, fee_amount, total_deducted;
+
+    // Prompt the user for inputs
+    printf("Enter amount to send: ");
+    scanf("%lf", &amount);
+
+    printf("Enter transaction fee (%%): ");
+    scanf("%lf", &fee_percentage);
+
+    // Calculate fee and total deduction
+    fee_amount = (fee_percentage / 100.0) * amount;
+    total_deducted = amount + fee_amount;
+
+    // Display results
+    printf("------Transaction summary-------");
+    printf("Transaction fee: %.2f UGX\n", fee_amount);
+    printf("Total deducted: %.2f UGX\n", total_deducted);
 
     return 0;
 }
