8786483b7c94e7d1c4493bdc7cc77a74cab87cc5
bujingodavidb-ship-it <bujingodavidb@gmail.com>
2026-03-24 19:30:27 +0300

    Update ex2_fuel.c

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,24 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
-
+/* Name: Bujingo DavidKabanda*/
+/* Student Number: 25/U/BIE/01364/PE */
 #include <stdio.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main() {
+    double distance, fuel, efficiency;
+
+    // Prompt user for input
+    printf("Enter distance (km): ");
+    scanf("%lf", &distance);
+
+    printf("Enter fuel used (litres): ");
+    scanf("%lf", &fuel);
+
+    // Calculate fuel efficiency
+    if (fuel != 0) {
+        efficiency = distance / fuel;
+        printf("Fuel efficiency: %.2f km/l\n", efficiency);
+    } else {
+        printf("Fuel used cannot be zero.\n");
+    }
 
     return 0;
 }
